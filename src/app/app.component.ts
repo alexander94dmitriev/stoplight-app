e609111b2a6ce14e14711d79bc41d26e6c17d6ac
Alexander Dmitriev <alexander94dmitriev>
2021-07-02 13:49:02 -0700

    Added code for challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subscription, timer} from "rxjs";
+import {map, take} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stoplight-app';
+export class AppComponent implements OnInit{
+  private colorIndexMap: Map<number, number>;
+  public timerInputValue: number;
+  private counter$: Observable<number>;
+  private subscriber: Subscription;
+  public colorIndex: number;
+
+  constructor() {
+    this.timerInputValue = 1;
+    this.counter$ = new Observable();
+    this.subscriber = new Subscription();
+    this.colorIndex = 0;
+    this.colorIndexMap = new Map<number, number>();
+    this.colorIndexMap.set(0,1); //white -> red
+    this.colorIndexMap.set(1,2); //red -> yellow
+    this.colorIndexMap.set(2,3); //yellow -> green
+    this.colorIndexMap.set(3,1); //green -> red
+  }
+  ngOnInit() { }
+
+  private resetSubscription() {
+    if (!this.subscriber.closed) {
+      this.subscriber.unsubscribe();
+      this.colorIndex = 0;
+    }
+  }
+
+  /*
+  The main function to switch the light using timer.
+   */
+  switchLightWithTimer() {
+    // Reset the subscriber to make sure it won't emmit any values from previous run.
+    this.resetSubscription();
+
+    // Create a timer observable based on the interval value user chose (in seconds).
+    this.counter$ = timer(0,this.timerInputValue*1000);
+
+    // Subscribe to the counter and switch the color after every tick.
+    this.subscriber = this.counter$.subscribe(val =>  {
+      this.colorIndex = <number>this.colorIndexMap.get(this.colorIndex);
+      console.log(this.colorIndex);
+    });
+
+  }
+
+  /*
+    Switch the light using button
+  */
+  switchLightManual() {
+    // Reset the subscriber to make sure it won't emmit any values from previous run.
+    this.resetSubscription();
+
+    this.colorIndex = <number>this.colorIndexMap.get(this.colorIndex);
+    console.log(this.colorIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
